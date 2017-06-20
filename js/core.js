4898f9011005d273fdaf43a81e3b3c712a546f90
Erjan Maksatbekov <mambo1234554321@gmail.com>
2017-06-20 11:57:51 +0600

    The End

--- js/core.js
+++ js/core.js
@@ -41,4 +41,39 @@ $(function() {
 			}
 		]
 	});
+	$('.slide').slick({
+		dots: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		responsive: [
+		{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+				infinite: true,
+				dots: false
+			}
+		},
+		{
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 2,
+				dots: false,
+				slidesToScroll: 2
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+				dots: false,
+				slidesToScroll: 1
+			}
+		}
+		]
+	});
+
 });
